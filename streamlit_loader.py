0b1add25a48d619ceb9206a9fa669ff5018652fc
drodutola <141102615+drodutola@users.noreply.github.com>
2024-08-03 16:46:12 -0500

    Update streamlit_loader.py

--- streamlit_loader.py
+++ streamlit_loader.py
@@ -2,15 +2,13 @@ import os
 import streamlit as st
 
 # Get the app content from the environment variable
-app_content = os.environ.get('APP_PY_CONTENT')
+app_content = st.secrets["APP_PY_CONTENT"]
 
 # Write the content to a temporary app.py file
 if app_content:
     with open('app.py', 'w') as f:
         f.write(app_content)
 
-# Now you can import and use your app as usual
-from app import your_main_function
 
-# Run your main function
-your_main_function()
+# Execute the app.py content
+exec(open('app.py').read())
